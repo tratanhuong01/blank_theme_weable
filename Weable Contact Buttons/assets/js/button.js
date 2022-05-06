b1f3b2169525049b152e6562ad516ddb45fc8f20
tratanhuong01 <tratanhuong01@gmail.com>
2022-05-06 15:37:21 +0700

    add form callback and form emal add aniamtion button

--- Weable Contact Buttons/assets/js/button.js
+++ Weable Contact Buttons/assets/js/button.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
-    const timeOutPopupContact = () => {
-        $('.popup__contact--content').removeClass('hidden');
-        $('.popup__contact--loading').addClass('hidden');
-        clearTimeout(timeOutPopupContact);
-    }
+    let timeOutPopupContact;
+    let timeOutError;
     const closePopupContact = () => {
+        $('#popup__contact--footer--checkbox').prop('checked', false);
         $('.button-fixed-comment').removeClass('hidden');
         $('.button-fixed-close').addClass('hidden');
         $('.popup__contact').removeClass('active');
@@ -17,15 +15,56 @@ $(document).ready(function () {
         const $target = $(event.target);
         if (!$target.closest('.popup__contact').length) {
             if ($('.button-fixed-close').hasClass('hidden')) {
+                $('.popup__contact--get').addClass('hidden');
                 $('.button-fixed-close').removeClass('hidden');
                 $('.button-fixed-comment').addClass('hidden');
                 $('.popup__contact').addClass('active');
-                setTimeout(timeOutPopupContact, 1000);
+                timeOutPopupContact = setTimeout(() => {
+                    $('.popup__contact--content').removeClass('hidden');
+                    $('.popup__contact--loading').addClass('hidden');
+                    clearTimeout(timeOutPopupContact);
+                }, 1000);
             }
             else {
                 closePopupContact();
             }
         }
+    });
+
+    const statusClickPopupContact = () => {
+        $('.popup__contact--content a').each(function () {
+            $(this).click((event) => {
+                if (!$('#popup__contact--footer--checkbox').is(":checked")) {
+                    event.preventDefault();
+                    $('.popup__contact--footer').removeClass('active');
+                    clearTimeout(timeOutError);
+                    $('.popup__contact--footer').addClass('active');
+                    timeOutError = setTimeout(() => {
+                        $('.popup__contact--footer').removeClass('active');
+                        clearTimeout(timeOutError);
+                    }, 300);
+                }
+                else {
+                    const attr = $(this).attr('href');
+                    if (typeof attr === 'undefined' || attr === false) {
+                        clearTimeout(timeOutPopupContact);
+                        $('.popup__contact--get').addClass('hidden');
+                        $('.popup__contact--loading').removeClass('hidden');
+                        timeOutPopupContact = setTimeout(() => {
+                            $(`.popup__contact--${$(this).data('type')}`).removeClass('hidden');
+                            $('.popup__contact--loading').addClass('hidden');
+                            clearTimeout(timeOutPopupContact);
+                        }, 1000);
+                    }
+                }
+            });
+        });
+    }
+
+    statusClickPopupContact();
+
+    $('#popup__contact--footer--checkbox').change(function (event) {
+        statusClickPopupContact(event.target.value)
     });
 
     $('.popup__contact--top--close').click(function () {
